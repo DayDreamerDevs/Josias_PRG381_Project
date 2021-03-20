7407f77be15ca4f03347344688918ee1ba09d43e
DayDreamerDevs <rynoreyneke54@gmail.com>
2021-03-20 21:52:09 +0200

    Changed Main() class name

--- src/PresentationLayer/DeliciousCatering.java
+++ src/PresentationLayer/DeliciousCatering.java
@@ -1,4 +1,6 @@
-public class App {
+package PresentationLayer;
+
+public class DeliciousCatering {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
