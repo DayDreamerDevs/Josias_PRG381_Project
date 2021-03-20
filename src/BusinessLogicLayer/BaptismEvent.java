27165b49240a4eea9f3f7413191ed55275833f8c
DayDreamerDevs <rynoreyneke54@gmail.com>
2021-03-20 21:49:06 +0200

    Extended the EventType class

--- src/BusinessLogicLayer/BaptismEvent.java
+++ src/BusinessLogicLayer/BaptismEvent.java
@@ -1,6 +1,16 @@
 package BusinessLogicLayer;
 
-public class BaptismEvent {
+public class BaptismEvent extends EventType{
+    @Override
+    public void getEventDetails() {
+        // TODO Auto-generated method stub
+        
+    }
 
+    @Override
+    public void eventTotal(int adultNum, int childNum, boolean confirmedEarly) {
+        // TODO Auto-generated method stub
+        
+    }
     
 }
