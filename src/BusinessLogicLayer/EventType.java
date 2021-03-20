e8fd595949d1e4ed5acc412db81b5159a2774857
DayDreamerDevs <rynoreyneke54@gmail.com>
2021-03-20 21:45:02 +0200

    Added abstract methods to EventType

--- src/BusinessLogicLayer/EventType.java
+++ src/BusinessLogicLayer/EventType.java
@@ -1,6 +1,11 @@
 package BusinessLogicLayer;
 
-public class EventType {
-
+public abstract class EventType {
+    protected double eventRate;
+    protected double adultRate;
+    protected double childRate;
     
+    public abstract void getEventDetails();
+
+    public abstract void eventTotal(int adultNum, int childNum, boolean confirmedEarly);
 }
