ff4c0b96f242fa839b80b0b8e19187a555ab7f18
DayDreamerDevs <rynoreyneke54@gmail.com>
2021-03-21 21:55:34 +0200

    Added exceptions handling

--- src/DataAccessLayer/DataHandler.java
+++ src/DataAccessLayer/DataHandler.java
@@ -13,8 +13,8 @@ public class DataHandler {
     public String sourceFile = null;
     public String sourceLine = null;
 
-    public List<String> readText(int index){
-        BufferedReader reader = new BufferedReader(new FileReader(new File(sourceFile)))
+    public List<String> readText(int index) throws IOException{
+        BufferedReader reader = new BufferedReader(new FileReader(new File(sourceFile)));
         List<String> clientList = new ArrayList<String>();
         List<String> eventList = new ArrayList<String>();
         List<String> ownerList = new ArrayList<String>();
@@ -25,26 +25,32 @@ public class DataHandler {
                 while ((sourceLine = reader.readLine()) != null) {
                     clientList.add(sourceLine);
                 }
+                reader.close();
                 return clientList;
-                break;
             case 1:
                 sourceFile = "event.txt";
                 while ((sourceLine = reader.readLine()) != null) {
                     eventList.add(sourceLine);
                 }
+                reader.close();
                 return eventList;
-                break;
             case 2:
                 sourceFile = "owner.txt";
                 while ((sourceLine = reader.readLine()) != null) {
                     ownerList.add(sourceLine);
                 }
+                reader.close();
                 return ownerList;
-                break;
             default:
+                sourceFile = "event.txt";
+                while ((sourceLine = reader.readLine()) != null) {
+                    eventList.add(sourceLine);
+                }
+                reader.close();
                 break;
+            }
+            return eventList;
         }
-    }
     
     public void writeText(int index, String writeString) throws IOException{
         BufferedWriter writer = new BufferedWriter(new FileWriter(new File(sourceFile)));
