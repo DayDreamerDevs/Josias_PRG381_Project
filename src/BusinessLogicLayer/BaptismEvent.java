280d6120eab9cbb17e1291d50ccb60aa4a1aeca8
DayDreamerDevs <rynoreyneke54@gmail.com>
2021-03-22 16:32:47 +0200

    Added input for the event form

--- src/BusinessLogicLayer/BaptismEvent.java
+++ src/BusinessLogicLayer/BaptismEvent.java
@@ -14,7 +14,30 @@ public class BaptismEvent extends EventType{
         System.out.println("What is the Event Date and Time of the Event?");
         event.setDateTime(sc.nextLine());
         
+        System.out.println("What is the Address of the Event Venue?");
+        event.setAddress(sc.nextLine());
         
+        System.out.println("What is the Total Number of people coming to the Event?");
+        event.setTotalPeople(sc.nextInt());
+
+        System.out.println("What is the Number od Adults coming to the Event?");
+        event.setAdultNum(sc.nextInt());
+
+        System.out.println("What is the Number of Children coming to the Event?");
+        event.setChildNum(sc.nextInt());
+
+        System.out.println("True or False is Decorations needed?");
+        event.setDecorationAns(sc.nextBoolean());
+
+        System.out.println("What is the Theme or Description of the decorations?");
+        event.setDateTime(sc.nextLine());
+
+        System.out.println("True or False is the event confirmed?");
+        event.setConfirmedAns(sc.nextBoolean());
+
+        System.out.println("What is the Confirmed Date and Time of the Event?");
+        event.setConfirmedDateTime(sc.nextLine());
+
         /*Owner owner = new Owner();
         owner.setAge(30);*/
     }
