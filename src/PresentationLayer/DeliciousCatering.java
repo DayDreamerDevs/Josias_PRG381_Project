0f58d12069403f6d3c094a6a0b0cd3054293eaf7
DayDreamerDevs <rynoreyneke54@gmail.com>
2021-03-22 20:43:40 +0200

    Removed unnecessary Imports

--- src/PresentationLayer/DeliciousCatering.java
+++ src/PresentationLayer/DeliciousCatering.java
@@ -1,8 +1,5 @@
 package PresentationLayer;
 
-import BusinessLogicLayer.EventSelection;
-import BusinessLogicLayer.EventType;
-
 public class DeliciousCatering {
     public static void main(String[] args) throws Exception {
         Register register = new Register();
