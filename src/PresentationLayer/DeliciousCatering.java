f89bee88ff24595aa71aaa17997a89ac3680955e
DayDreamerDevs <rynoreyneke54@gmail.com>
2021-03-22 19:47:13 +0200

    Linked Main and Register classes

--- src/PresentationLayer/DeliciousCatering.java
+++ src/PresentationLayer/DeliciousCatering.java
@@ -5,6 +5,9 @@ import BusinessLogicLayer.EventType;
 
 public class DeliciousCatering {
     public static void main(String[] args) throws Exception {
+        Register register = new Register();
+        register.registerScreen();
+        
         EventSelection eventSelection = new EventSelection();
         EventType eventType = eventSelection.getEvent(1);
 
