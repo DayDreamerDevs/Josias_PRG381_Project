f9fe15dc819ef073e7575bcf3bed48935ec67164
DayDreamerDevs <rynoreyneke54@gmail.com>
2021-03-22 17:56:55 +0200

    Changed cellNumber to string

--- src/PresentationLayer/Register.java
+++ src/PresentationLayer/Register.java
@@ -2,6 +2,8 @@ package PresentationLayer;
 
 import java.util.*;
 
+import BusinessLogicLayer.User;
+
 public class Register {
     public void registerScreen(){
         Scanner sc = new Scanner(System.in);
@@ -10,6 +12,51 @@ public class Register {
         System.out.println("Do you already have an Account?");
         
         int accountYN = sc.nextInt();
+
+        if (accountYN == 1) {
+            Login login = new Login();
+            login.loginScreen();
+        } else {
+            User user = new User();
+            Random random = new Random();
+
+            char char1 = (char) ('a' + random.nextInt(26));
+            char char2 = (char) ('a' + random.nextInt(26));
+    
+            int random_int1 = random.nextInt(9);
+            int random_int2 = random.nextInt(9);
+            int random_int3 = random.nextInt(9);
+    
+           //Getting input and setting values
+            String clientID = new StringBuilder().append("bi" + char1 + char2 + random_int1 + random_int2 + random_int3).toString();
+            user.setClientID(clientID);
+
+            System.out.println("What is your Username?");
+            user.setUsername(sc.nextLine());
+
+            System.out.println("What is your Password?");
+            user.setPassword(sc.nextLine());
+
+            System.out.println("What is your First Name?");
+            user.setFirstName(sc.nextLine());
+
+            System.out.println("What is your Last Name?");
+            user.setLastName(sc.nextLine());
+
+            System.out.println("What is your Age?");
+            user.setAge(sc.nextInt());
+
+            while (sc.nextLine().length() != 10) {
+                System.out.println("What is your Cell Number?");
+                user.setCellNumber(sc.nextLine());
+            }
+
+            System.out.println("What is your Email address?");
+            user.setEmail(sc.nextLine());
+
+            System.out.println("What is your National ID?");
+            user.setNationalID(sc.nextLine());
+        }
         
     }
 }
