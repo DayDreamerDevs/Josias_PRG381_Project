0bd87793bd10e9ad5461647e1267943efce4f95c
DayDreamerDevs <rynoreyneke54@gmail.com>
2021-03-22 20:36:17 +0200

    Removed initialization for eventTyped

--- src/PresentationLayer/DeliciousCatering.java
+++ src/PresentationLayer/DeliciousCatering.java
@@ -7,14 +7,5 @@ public class DeliciousCatering {
     public static void main(String[] args) throws Exception {
         Register register = new Register();
         register.registerScreen();
-        
-        EventSelection eventSelection = new EventSelection();
-        EventType eventType = eventSelection.getEvent(1);
-
-        int adultNum = 2, childNum= 2;
-        boolean confirmedEarly = true;
-
-        eventType.getEventDetails();
-        eventType.eventTotal(adultNum, childNum, confirmedEarly);
     }
 }
