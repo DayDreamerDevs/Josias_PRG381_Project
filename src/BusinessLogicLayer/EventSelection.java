160c20956163c5f6c8574b291fa3923ec8f3e47d
DayDreamerDevs <rynoreyneke54@gmail.com>
2021-03-22 15:28:51 +0200

    added instance to the event factory

--- src/BusinessLogicLayer/EventSelection.java
+++ src/BusinessLogicLayer/EventSelection.java
@@ -1,7 +1,26 @@
 package BusinessLogicLayer;
 
 public class EventSelection {
-    public void getEvent(){
+    EventType eventType;
+    public EventType getEvent(int typeOfEvent){
         
+        switch (typeOfEvent) {
+            case 1:
+                eventType = new BaptismEvent();
+                break;
+            case 2:
+                eventType = new WeddingEvent();
+                break;
+            case 3:
+                eventType = new YearEndFunction();
+                break;
+            case 4:
+                eventType = new BirthdayEvent();
+                break;
+        
+            default:
+                break;
+        }
+        return eventType;
     }
 }
--- src/PresentationLayer/DeliciousCatering.java
+++ src/PresentationLayer/DeliciousCatering.java
@@ -1,7 +1,11 @@
 package PresentationLayer;
 
+import BusinessLogicLayer.EventSelection;
+import BusinessLogicLayer.EventType;
+
 public class DeliciousCatering {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        EventSelection eventSelection = new EventSelection();
+        EventType eventType = eventSelection.getEvent(1);
     }
 }
