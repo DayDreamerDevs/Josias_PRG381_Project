f4d621e4b15e80ae449383119a2cf6f7458e24f9
DayDreamerDevs <rynoreyneke54@gmail.com>
2021-03-22 21:03:44 +0200

    Added in Login logic

--- src/PresentationLayer/Login.java
+++ src/PresentationLayer/Login.java
@@ -3,6 +3,9 @@ package PresentationLayer;
 import java.io.IOException;
 import java.util.*;
 
+import BusinessLogicLayer.User;
+import DataAccessLayer.DataHandler;
+
 public class Login {
     public void loginScreen() throws IOException{
         Scanner sc = new Scanner(System.in);
@@ -14,13 +17,32 @@ public class Login {
         String password = sc.nextLine();
 
         boolean isOwner = false;
-        if (isOwner == false) {
-            Menu menu = new Menu();
-            menu.menuScreen();
-        }else{
-            OwnerMenu ownerMenu = new OwnerMenu();
-            ownerMenu.ownerMenuScreen();
+        List<User> userList = new ArrayList<User>();
+        DataHandler dataHandler = new DataHandler();
+
+        userList = dataHandler.readText(0);
+
+        List usernameList = userList.subList(1, 1);
+        List passwordList = userList.subList(2, 2);
+        boolean validLogin = false;
+
+        while (validLogin == false) {
+            if (username == userList.toString() && password == passwordList.toString()) {
+                validLogin = true;
+                if (username == "Raheal " && password == "RahealPass121") {
+                    isOwner = true;
+                }
+                if (isOwner == true) {
+                    OwnerMenu ownerMenu = new OwnerMenu();
+                    ownerMenu.ownerMenuScreen();
+                }else{
+                    Menu menu = new Menu();
+                    menu.menuScreen();
+                }
+            } else{
+                System.out.println("Please enter a Valid Username or Password");
+            } 
         }
-        
+
     }
 }
