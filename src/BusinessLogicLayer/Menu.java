ea1597e8b1f482b5bc1e0b3398a9249d3438772b
DayDreamerDevs <rynoreyneke54@gmail.com>
2021-03-22 18:48:31 +0200

    added properties and constructors

---
+++ src/BusinessLogicLayer/Menu.java
@@ -0,0 +1,57 @@
+package BusinessLogicLayer;
+
+public class Menu {
+    private String menuID;
+    private boolean adult;
+    private boolean kids;
+    private boolean warmsDrinks;
+    private boolean coldDrinks;
+    private boolean desert;
+    
+    public String getMenuID() {
+        return menuID;
+    }
+    public void setMenuID(String menuID) {
+        this.menuID = menuID;
+    }
+    public boolean isAdult() {
+        return adult;
+    }
+    public void setAdult(boolean adult) {
+        this.adult = adult;
+    }
+    public boolean isKids() {
+        return kids;
+    }
+    public void setKids(boolean kids) {
+        this.kids = kids;
+    }
+    public boolean isWarmsDrinks() {
+        return warmsDrinks;
+    }
+    public void setWarmsDrinks(boolean warmsDrinks) {
+        this.warmsDrinks = warmsDrinks;
+    }
+    public boolean isColdDrinks() {
+        return coldDrinks;
+    }
+    public void setColdDrinks(boolean coldDrinks) {
+        this.coldDrinks = coldDrinks;
+    }
+    public boolean isDesert() {
+        return desert;
+    }
+    public void setDesert(boolean desert) {
+        this.desert = desert;
+    }
+    public Menu() {
+    }
+    public Menu(String menuID, boolean adult, boolean kids, boolean warmsDrinks, boolean coldDrinks, boolean desert) {
+        this.menuID = menuID;
+        this.adult = adult;
+        this.kids = kids;
+        this.warmsDrinks = warmsDrinks;
+        this.coldDrinks = coldDrinks;
+        this.desert = desert;
+    }
+}
