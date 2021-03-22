265ceb51aa178f113bc2e469c743d90d22f2d2d7
DayDreamerDevs <rynoreyneke54@gmail.com>
2021-03-22 15:01:56 +0200

    Added output to login and register classes

---
+++ src/PresentationLayer/Register.java
@@ -0,0 +1,15 @@
+package PresentationLayer;
+
+import java.util.*;
+
+public class Register {
+    public void registerScreen(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("{-----Welcome to Delicious Catering Service-----}");
+        System.out.println("Do you already have an Account?");
+        
+        int accountYN = sc.nextInt();
+        
+    }
+}
